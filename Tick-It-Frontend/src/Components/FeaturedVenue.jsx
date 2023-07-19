486ea95252c9a92256ce43c5d117c17ca98f0693
John Brooks <johnrbrooks93@gmail.com>
2023-07-19 11:56:30 -0400

    Featured Venue and Featured Band prepared

--- Tick-It-Frontend/src/Components/FeaturedVenue.jsx
+++ Tick-It-Frontend/src/Components/FeaturedVenue.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export default function FeaturedVenue() {
+
+    const navigate = useNavigate()
+
+    const[featuredVenue, setFeaturedVenue] = useState({})
+
+    useEffect(() => {
+        const getFeaturedVenue = async () => {
+            const response = await axios.get()
+            setFeaturedVenue(response.data)
+        }
+        getFeaturedVenue()
+    }, [])
+
+    const venueDetails = (id) => {
+        console.log('Go to the venue...')
+    }
+
     return(
-        <div className='featured-venue-container'>
-            <h1>Featured Venue</h1>
+        <div className='container-fluid w-50'>
+            <div className="text-center">
+                <h1>Featured Venue</h1>
+            </div>
+            <div class="card d-flex align-items-center justify-content-center">
+                <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary" onClick={() => venueDetails()}>Show Details</a>
+                </div>
+            </div>
         </div>
     )
 }
