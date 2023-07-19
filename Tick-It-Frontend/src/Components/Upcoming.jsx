1c2f4846ea1de56fb90f57e60504c932ba00bb72
Steven Underwood <stevenunderwood2@hotmail.com>
2023-07-19 10:00:28 -0500

    Create Upcoming skeleton

--- Tick-It-Frontend/src/Components/Upcoming.jsx
+++ Tick-It-Frontend/src/Components/Upcoming.jsx
@@ -1,7 +1,44 @@
+import {useState, useEffect} from 'react'
+import { useNavigate} from 'react-router-dom'
+import axios from 'axios'
+
+
 export default function Upcoming() {
-    return(
-        <div className='upcoming-container'>
-            <h1>Upcoming</h1>
-        </div>
-    )
+
+    const navigate = useNavigate()
+    const [upcoming, setUpcoming] = useState([])
+
+    useEffect(() => {
+        const getUpcoming = async () => {
+            const response = await axios.get()
+            setUpcoming(response.data)
+        }
+        getUpcoming()
+    }, [])
+
+    const showDetails = (id) => {
+        navigate(`/showdetails/{id}`)
+    }
+
+    if (upcoming.length === 0) {
+        return (
+            <h1>Wow, This is taking a while!</h1>
+        )
+    }
+    else {
+        return(
+            <div className='upcoming-container'>
+                <div className='row'>
+                    <h1>Upcoming Events:</h1>
+                </div>
+                <div className='row'>
+                    {shows.map((show, index) =>
+                        <div className='col' key={index}>
+                            <img src={show.poster.url} alt="show.name" onClick={() => showDetails(show.id)} />
+                        </div>
+                    )}
+                </div>
+            </div>
+        )
+    }
 }
