19b658c4cdc714354d717015ce8f49aa110df15e
John Brooks <johnrbrooks93@gmail.com>
2023-07-19 10:35:51 -0400

    Generic navbar built

--- Tick-It-Frontend/src/Components/Nav.jsx
+++ Tick-It-Frontend/src/Components/Nav.jsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom'
+
 export default function Nav() {
     return(
-        <nav className="navbar navbar-light bg-primary">
-            
+        <nav className="navbar navbar-expand bg-primary">
+            <div className="container-fluid justify-content-evenly">
+                <div className="d-flex">
+                    <Link to="/" className="btn btn-dark ms-2">Home</Link>
+                </div>
+                <Link to="/admin" className="navbar-brand fs-2">Tick-It</Link>
+                <form class="d-flex" role="search">
+                    <input className="form-control" placeholder="Search" type="search" />
+                    <button className="btn btn-dark mx-2" type="submit">Search</button>
+                </form>
+            </div>
         </nav>
     )
 }
