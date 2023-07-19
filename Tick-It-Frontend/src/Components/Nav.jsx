21d32dbe4c23e50ebf4c57e1afda1b5524390bf1
John Brooks <johnrbrooks93@gmail.com>
2023-07-19 10:58:24 -0400

    Navbar rebuilt

--- Tick-It-Frontend/src/Components/Nav.jsx
+++ Tick-It-Frontend/src/Components/Nav.jsx
@@ -2,17 +2,21 @@ import { Link } from 'react-router-dom'
 
 export default function Nav() {
     return(
-        <nav className="navbar navbar-expand bg-primary">
-            <div className="container-fluid justify-content-evenly">
-                <div className="d-flex">
-                    <Link to="/" className="btn btn-dark ms-2">Home</Link>
+        <div class="container-fluid">
+            <div class="row align-items-center bg-primary py-3">
+                <div class="col-3 d-flex justify-content-center">
+                    <Link to="/" className="btn btn-dark">Home</Link>
                 </div>
-                <Link to="/admin" className="navbar-brand fs-2">Tick-It</Link>
-                <form class="d-flex" role="search">
-                    <input className="form-control" placeholder="Search" type="search" />
-                    <button className="btn btn-dark mx-2" type="submit">Search</button>
-                </form>
+                <div class="col-3 d-flex justify-content-end">
+                    <Link to="/admin" className="navbar-brand fs-2">Tick-It</Link>
+                </div>
+                <div class="col-6 d-flex justify-content-end">
+                    <form class="d-flex" role="search">
+                        <input className="form-control" placeholder="Search" type="search" />
+                        <button className="btn btn-dark mx-2" type="submit">Search</button>
+                    </form>
+                </div>
             </div>
-        </nav>
+        </div>
     )
 }
